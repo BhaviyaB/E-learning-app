39455c033113f8aee7b27d8e6e5344852ff20b51
Bhaviya Babu <bhaviya.babu@ratnaglobaltech.com>
2023-06-05 17:52:48 +0530

    updated api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
-from courses.models import Subject
-from courses.api.serializers import SubjectSerializer
-
+from courses.models import Subject, Course
+from courses.api.serializers import SubjectSerializer,CourseSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 class SubjectListView(generics.ListAPIView):
     queryset=Subject.objects.all()
@@ -10,3 +15,15 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset=Subject.objects.all()
     serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        authentication_classes = [BasicAuthentication]
+        permission_classes = [IsAuthenticated]
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+    
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
